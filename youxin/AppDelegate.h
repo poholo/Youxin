3bf16db525b8d00dedd7ef1cc95d18dfb37d4f63
fei <mac@localhost.(none)>
2013-09-08 18:01:54 +0800

    Initial Commit

---
+++ youxin/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  youxin
+//
+//  Created by fei on 13-9-8.
+//  Copyright (c) 2013年 mjc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
